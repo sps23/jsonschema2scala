bb24593b5d970c08b9c14ba6b724d4b5f01d592a
Sylwester Stocki <sylwester.stocki.rnd@gmail.com>
2020-01-26 23:48:26 +0000

    Regenerating collateral.json

--- src/main/scala/jsonschema2scala/generated/collateral/Collateral.scala
+++ src/main/scala/jsonschema2scala/generated/collateral/Collateral.scala
@@ -1,26 +1,25 @@
-package jsonschema2scala.generated.collateral
+package jsonschema2scala.generated
+package collateral
 
 import java.time.LocalDateTime
-
-import jsonschema2scala.generated.{CurrencyCode, RegulatoryBook}
 
 case class Collateral(
     id: String,
     date: LocalDateTime,
-    loanIds: Option[List[String]],
-    accountIds: Option[List[String]],
+    loan_ids: Option[List[String]],
+    account_ids: Option[List[String]],
     charge: Option[Int],
-    currencyCode: Option[CurrencyCode],
-    encumbranceAmount: Option[Int],
-    encumbranceType: Option[CollateralEncumbranceType],
-    endDate: Option[LocalDateTime],
-    regulatoryBook: Option[RegulatoryBook],
+    currency_code: Option[CurrencyCode],
+    encumbrance_amount: Option[Int],
+    encumbrance_type: Option[CollateralEncumbranceType],
+    end_date: Option[LocalDateTime],
+    regulatory_book: Option[RegulatoryBook],
     source: Option[String],
-    startDate: Option[LocalDateTime],
+    start_date: Option[LocalDateTime],
     `type`: Option[CollateralType],
     value: Int,
-    valueDate: Option[LocalDateTime],
-    versionId: Option[String],
-    volAdj: Option[BigDecimal],
-    volAdjFx: Option[BigDecimal]
+    value_date: Option[LocalDateTime],
+    version_id: Option[String],
+    vol_adj: Option[BigDecimal],
+    vol_adj_fx: Option[BigDecimal]
 )
