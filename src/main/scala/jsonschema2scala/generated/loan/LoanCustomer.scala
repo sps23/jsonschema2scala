38f17cd38869f2697ba2210212e7728798dd9fad
Sylwester Stocki <sylwester.stocki.rnd@gmail.com>
2021-02-28 20:48:58 +0000

    Deleting old generated Fire Schemas.

--- src/main/scala/jsonschema2scala/generated/loan/LoanCustomer.scala
+++
@@ -1,6 +0,0 @@
-package jsonschema2scala.generated.loan
-
-case class LoanCustomer(
-    id: String,
-    income_amount: Int
-)
--- src/main/scala/jsonschema2scala/generated/package.scala
+++
@@ -1,7 +0,0 @@
-package jsonschema2scala
-
-package object generated {
-
-  type MicCode = String
-  type Ccf     = BigDecimal
-}
